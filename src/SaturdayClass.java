0561124e1c11f462421d3c4f490d7d12e0ca0d3b
Anjola Omole <anjola567@gmail.com>
2023-08-06 18:51:30 -0500

    Changes to Main Code

--- src/SaturdayClass.java
+++ src/SaturdayClass.java
@@ -4,14 +4,14 @@ public class SaturdayClass {
     public static void main(String[] args) {
 
 
-        System.out.println("Please enter your height in iniches");
+        System.out.println("Please enter your height in inches");
 
-        Scanner scanner = new scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         int input = scanner.nextInt();
 
         if (input < 60){
-            System.out.println("You are classifed as short");
+            System.out.println("You are classified as short");
         } else if( input > 60 && input <72){
             System.out.println("You are Medium height");
         } else{
