2debc45215f7f73ba0f045fe4e27668bf761c9c6
Anjola Omole <anjola567@gmail.com>
2023-08-06 18:46:34 -0500

    First Assignment

--- src/SaturdayClass.java
+++ src/SaturdayClass.java
@@ -1,7 +1,22 @@
-
+import java.util.Scanner;
 
 public class SaturdayClass {
     public static void main(String[] args) {
+
+
+        System.out.println("Please enter your height in iniches");
+
+        Scanner scanner = new scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        if (input < 60){
+            System.out.println("You are classifed as short");
+        } else if( input > 60 && input <72){
+            System.out.println("You are Medium height");
+        } else{
+            System.out.println("You are tall");
+        }
 
 
 
